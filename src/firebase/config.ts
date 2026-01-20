442d0e681cb0068a5ee62c39fb0b297c83c2a565
Raven-Music <jyotitallapaneni15@gmail.com>
2026-01-20 16:17:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2128195381-246e0",
+  "appId": "1:969227198560:web:255a1a5dc945d7abf72305",
+  "apiKey": "AIzaSyCrFFqULqeGv3DpF-tXMnzgfiTBpwMhFkU",
+  "authDomain": "studio-2128195381-246e0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "969227198560"
+};
